8ad51aaf22572835e6666cae201723ab8c81d6f9
Anhquann <quananhle188@gmail.com>
2025-05-06 21:01:59 +0700

    Update ID_EX_register.v

--- ID_EX_register.v
+++ ID_EX_register.v
@@ -1,5 +1,5 @@
 module ID_EX_register (
-	input MemReadD, MemWriteD, ALUSrcD, JumpD, RegWriteD, BranchD, MuxjalrD, clk, reset,
+	input MemReadD, MemWriteD, ALUSrcD, JumpD, RegWriteD, BranchD, MuxjalrD, Stall, clk, reset,
 	input [3:0] ALUOpD,
 	input [2:0] ImmControlD, WriteBackD, funct3D,
 	input [31:0] RD1D, RD2D, PCD, 
@@ -21,6 +21,8 @@ module ID_EX_register (
 			RdE <= 5'd0; ImmExtE <= 32'd0; PCPlus4E <= 32'd0; funct3E <= 3'b000; Rs1E <= 5'd0; Rs2E <= 5'd0;
 		end
 		else begin
+			if (Stall) begin RegWriteE <= 0; MemWriteE <= 0; end
+			else begin
 			MemReadE <= MemReadD;
 			MemWriteE <= MemWriteD;
 			ALUSrcE <=  ALUSrcD;
@@ -38,7 +40,7 @@ module ID_EX_register (
 			ImmExtE <= ImmExtD;
 			PCPlus4E <= PCPlus4D;
 			funct3E <= funct3D;
-			Rs1E <= Rs1D; Rs2E <= Rs2D;
+			Rs1E <= Rs1D; Rs2E <= Rs2D; end
 		end
 	end
 endmodule 
