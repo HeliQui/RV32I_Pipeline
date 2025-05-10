0bb1e371729efb9381b31d74ea2c829a30507a27
Anhquann <quananhle188@gmail.com>
2025-05-10 12:08:39 +0700

    Update WB_stage.v

--- WB_stage.v
+++ WB_stage.v
@@ -3,5 +3,5 @@ module WB_stage (
 	input [31:0] ALUResultW, ReadDataW, PCTargetW, ImmExtW, PCPlus4W,
 	output [31:0] ResultW
 );
-	mux5_1 (WriteBackW, ALUResultW, ReadDataW, PCPlus4W, ImmExtW, PCTargetW, ResultW);
+	mux5_1 mux51 (WriteBackW, ALUResultW, ReadDataW, PCPlus4W, ImmExtW, PCTargetW, ResultW);
 endmodule 
