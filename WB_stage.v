fdde7fda035123b7855ffa317176e18730415815
ntqui0912 <23521299@gm.uit.edu.vn>
2025-05-10 14:03:23 +0700

    Xoa may stage

--- WB_stage.v
+++
@@ -1,7 +0,0 @@
-module WB_stage ( 
-	input [2:0] WriteBackW,
-	input [31:0] ALUResultW, ReadDataW, PCTargetW, ImmExtW, PCPlus4W,
-	output [31:0] ResultW
-);
-	mux5_1 mux51 (WriteBackW, ALUResultW, ReadDataW, PCPlus4W, ImmExtW, PCTargetW, ResultW);
-endmodule 
